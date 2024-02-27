b5cd1d069ef3d2a6a6e68fcf669bd4af6a67fd6e
JOSEENCARNACION\jose_ <jose.encarnacion@bairesdev.com>
2024-02-27 14:45:10 -0400

    Add test case

--- DemoblazeProject/Tests/CartTests.cs
+++ DemoblazeProject/Tests/CartTests.cs
@@ -52,6 +52,40 @@ namespace DemoblazeProject.Tests
 
         [TestCategory("Cart Tests")]
         [TestMethod]
+        public void AddMultiplesProducts()
+        {
+            var Home = new HomePage(DefaultWait);
+            var SignUp = new SignUpPage(DefaultWait);
+            var Login = new LoginPage(DefaultWait);
+            var Product = new ProductPage(DefaultWait);
+            var randomguid = Guid.NewGuid();
+            var shortguid = randomguid.ToString().Substring(5, 7);
+            var username = "Jose" + shortguid;
+            var password = "secret" + shortguid;
+            List<string> products = new List<string>();
+            products.Add("Samsung galaxy s6");
+            products.Add("Nokia lumia 1520");
+            products.Add("Iphone 6 32gb");
+            Home.productStoreLogo.Displayed.Should().BeTrue();
+            Home.selectOption("Sign up");
+            SignUp.SignUp(username, password);
+            Home.selectOption("Log in");
+            Login.SignIn(username, password);
+            WebElementExtensions.WaitForSpinningWheel();
+            Home.validateOptionDisplayed("Welcome " + username).Should().BeTrue();
+            foreach (var product in products)
+            {
+                Home.selectProduct(product);
+                Product.productNameLabel.Text.Should().Contain(product);
+                Product.addProductToCartAndValidateInfoInCart().Should().BeTrue();
+                Home.selectOption("Home");
+            }
+            Home.selectOption("Cart");
+        }
+
+
+        [TestCategory("Cart Tests")]
+        [TestMethod]
         public void AddProducts_And_ValidateTotal()
         {
             var Home = new HomePage(DefaultWait);
